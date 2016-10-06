fe1f8dd32641666693fe6022183a5b0548cf1e49
Shane Skikne <shaneskikne@gmail.com>
2016-10-05 21:31:39 -0400

    Cleaned up a bit

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Software to make only the best of audio mashups (aka randomly create mashups)
 This incredibly complex algorithm uses a random number generator to create random mashups of songs. If it isn't clear, this took 30 minutes. Don't judge. 
 
 To run, download pydub http://pydub.com/ by running `pip install pydub`
+
+`python mashup_maker.py song1.mp3 song2.mp3`
+
+I have had no issues using mp3's and it should theoretically handle other file formats.  
+
+The resulting mashup will be as long as the shortest song. It can handle any number of songs as long as the shortest song isn't shorter than approximately 4 seconds multipled by the number of songs.  
--- mashup_maker.py
+++ mashup_maker.py
@@ -3,15 +3,17 @@ from pydub import AudioSegment
 import random 
 
 class Mashup_Maker():
+	
 	def __init__(self):
-		self.min_seg_len = 2
-		self.max_seg_len = 8
-		self.second = 1000
-		#TODO MAKE CONSTATS^
-
+		self.SECOND = 1000
+		self.MIN_SEG_LEN = 2
+		self.MAX_SEG_LEN = 8
+	
+		self.mashup_title = "mashup_"	
 		self.songs = []
 		for i in range(1, len(sys.argv)):
 			song_title  = sys.argv[i]
+			self.mashup_title += (song_title.split(".")[0] + "_")
 			self.songs.append(self.open_song(song_title))
 
 		self.get_min_len()
@@ -19,11 +21,13 @@ class Mashup_Maker():
 
 
 	def open_song(self, file_name):
+		# Opens the song file using pydub
 		s_type = file_name.split(".")[1]
 		s = AudioSegment.from_file(file_name, s_type)
 		return s
 
 	def get_min_len(self):
+		# Finds the song with the minimum length
 		self.min_len = len(self.songs[0])
 		self.short_song_index = 0 
 		for i in range(len(self.songs)):
@@ -31,7 +35,8 @@ class Mashup_Maker():
 				self.min_len = len(self.songs[i])
 				self.short_song_index = i
 		
-	def make(self):
+	def mash(self):
+		# Mashes random segments from each song
 		self.pos = 0
 		self.mashup = self.get_song_seg(0)
 		self.seg_count = 1
@@ -41,19 +46,21 @@ class Mashup_Maker():
 			seg = self.get_song_seg(curr_song_index)
 			self.seg_count +=1
 			self.mashup = self.mashup.append(seg, crossfade=100) 
-		self.mashup.export("mashup.mp3", format="mp3")
+		self.mashup.export(self.mashup_title + ".mp3", format="mp3")
 
 	def get_song_seg(self, song_index):
-		
-		seg_len = random.randint(self.min_seg_len,self.max_seg_len)*self.second
+		# Gets a random segment of the indexed song 
+		seg_len = random.randint(self.MIN_SEG_LEN, self.MAX_SEG_LEN)*self.SECOND
 		if self.pos + seg_len > self.min_len:
-				seg = self.songs[self.short_song_index][self.pos:]
+			# If this segment would put us over the end of the shortest song, finish the mashup with that song
+			seg = self.songs[self.short_song_index][self.pos:]
 		else:
+			# Grab a random segment from the song
 			seg = self.songs[song_index][self.pos:self.pos + seg_len]
 		self.pos += seg_len
 		return seg
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
-		mm = Mashup_Maker()
-		mm.make()
+		mashup_maker = Mashup_Maker()
+		mashup_maker.mash()
